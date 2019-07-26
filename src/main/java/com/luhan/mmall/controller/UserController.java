2ca2f41ba935642dade0a86ad120f8ef6fb45046
Luhan <765478939@qq.com>
2019-07-26 22:21:13 +0800

    添加tokenCache类  完善ServerResponse类

---
+++ src/main/java/com/luhan/mmall/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.luhan.mmall.controller;
+
+/**
+ * 用户模块API
+ * @author luHan
+ * @date 2019-07-25 21:42
+ */
+public class UserController {
+    //TODO 注册
+
+    //TODO 登录，将用户信息保存进session中
+
+    //TODO 登出
+
+    //TODO 获取注册设置的问题
+
+    //TODO 检查问题答案，返回用户Token
+
+    //TODO 忘记密码的重置密码，拿到用户TOken跟本地缓存进行比较,如果成功则允许修改，否则不允许修改
+
+    //TODO 登录状态下修改密码
+
+    //TODO 修改用户信息,用户名称不允许更新，并且需要判断email是否存在，不允许存在相同的email
+
+    //TODO 获取用户详细信息，需要登录
+}
